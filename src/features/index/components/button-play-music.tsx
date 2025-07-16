4c475054e3207617726a78ed73ad0d45de9770d6
Arahmaa <bimaaurasakti2001@gmail.com>
2025-07-16 20:32:28 +0700

    fix button song overflow

--- src/features/index/components/button-play-music.tsx
+++ src/features/index/components/button-play-music.tsx
@@ -17,14 +17,12 @@ export default function ButtonPlayMusic() {
   };
 
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 flex justify-center">
-      <div className='min-w-[440px] flex justify-end pt-4 pe-4'>
-        <Button variant="default" size="icon" className="size-8 rounded-full" onClick={toggleMute}>
-          {isMuted ? <VolumeOff /> : <Volume2 />}
-        </Button>
-      </div>   
+    <>
+      <Button variant="default" size="icon" className="fixed size-8 rounded-full top-4 right-4 z-10" onClick={toggleMute}>
+        {isMuted ? <VolumeOff /> : <Volume2 />}
+      </Button>
 
       <audio ref={audioRef} src="/songs/I_Still_Love_You.mp3" loop autoPlay />
-    </div>
+    </>
   );
 }
